0ceefdbeb0ee42edbbe1283e5994fdc12c7b6ef9
Jayden Chen <jayden.h.chen@gmail.com>
2026-09-17 21:40:01 -0400

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/jayden-chen-portfolio/' : '/',
+  base: command === 'build' ? '/portfolio/' : '/',
   plugins: [react()],
 }))
